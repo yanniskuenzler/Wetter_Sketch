16ca2a1108bcac53d74be4f6177155ea7551b0e8
Yannis Künzler <y.kuenzler@icloud.com>
2021-09-10 13:21:22 +0200

    Add Clouds, toggle Background Colour

--- WetterSketch_pde/WetterSketch_pde.js
+++ WetterSketch_pde/WetterSketch_pde.js
@@ -3,7 +3,8 @@ let rainStatus;
 
 function setup() {
   createCanvas(1200, 700);
-  background(3, 169, 252);
+  background(165, 198, 232);
+  frameRate(30);
 
   button = createButton('toggle rain');
   button.position(0, 0);
@@ -13,6 +14,10 @@ rainCheck();
 
 function draw() {
   rain();
+
+  createCloud(550, 100, 70);
+  createCloud(900, 250, 70);
+  createCloud(170, 100, 70);
 
   fill('green');
   stroke('green');
@@ -39,13 +44,32 @@ function rainCheck() {
 
 function rain() {
   if (rainStatus == 1) {
+    background(165, 198, 232)
+    let cloudColour;
+    cloudColour = random(90, 100);
     stroke(40, 3, 252);
     fill(40, 3, 252);
+    let count = 0
+    while (count <= 3){
     circle(random(0, 1200), random(0, 700), 7);
+    count += 1;
+    }
+    count = 0;
+    fill(cloudColour);
+    stroke(cloudColour);
   }
   else {
     clear();
     background(3, 169, 252)
+    fill('white');
+    stroke('white');
   }
 }
 
+function createCloud(x, y, r) {
+  circle(x, y, r);
+  circle(x + 30, y - 20, r - 20);
+  circle(x + 25, y, r + 15);
+  circle(x + 50, y - 5, r + 15);
+  circle(x + 85, y + 10, r - 20);
+}
