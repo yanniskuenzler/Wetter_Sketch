be65c73ac50d7a9eb064021b95e46d5cb22d06bd
Yannis Künzler <y.kuenzler@icloud.com>
2021-09-10 11:15:02 +0200

    Add Toggle Rain feature

--- WetterSketch_pde/WetterSketch_pde.js
+++ WetterSketch_pde/WetterSketch_pde.js
@@ -1,10 +1,18 @@
+let button;
+let rainStatus;
+
 function setup() {
   createCanvas(1200, 700);
   background(3, 169, 252);
+
+  button = createButton('toggle rain');
+  button.position(0, 0);
+  button.mousePressed(rainCheck);
 }
 
-
 function draw() {
+  rain();
+
   fill('green');
   stroke('green');
   rect(0, 500, 1200, 250); //Grass
@@ -13,8 +21,30 @@ function draw() {
   fill('brown');
   stroke('brown');
   rect(150, 300, 70, 250);
-
   fill('green');
   stroke('black');
   circle(185, 280, 250);
+
 }
+
+function rainCheck() {
+  if (rainStatus == 0) {
+    rainStatus = 1;
+  }
+  else {
+    rainStatus = 0;
+  }
+}
+
+function rain() {
+  if (rainStatus == 1) {
+    stroke(40, 3, 252);
+    fill(40, 3, 252);
+    circle(random(0, 1200), random(0, 700), 7);
+  }
+  else {
+    clear();
+    background(3, 169, 252)
+  }
+}
+
